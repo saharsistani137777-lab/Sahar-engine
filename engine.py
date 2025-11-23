8bfb662893f03ef5898d15d3cf41c8a363f596c6
Sahar Sistani <saharsistani137777@gmail.com>
2025-11-23 12:50:12 +0330

    engine.py

--- engine.py
+++ engine.py
@@ -1,1 +1,21 @@
+import sys
+from lexer import Lexer
+from parser import Parser
+from interpreter import Interpreter
+
+def run_file(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        src = f.read()
+    lexer = Lexer(src)
+    tokens = lexer.tokens()
+    parser = Parser(tokens)
+    program = parser.parse()  # فرض می‌کنیم parser کامل است
+    interp = Interpreter()
+    interp.eval(program)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python engine.py <file.se>")
+        sys.exit(1)
+    run_file(sys.argv[1])
 
