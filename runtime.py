697cc28cedccbf80df342910557f59f29bfb8bdb
Sahar Sistani <saharsistani137777@gmail.com>
2025-11-23 12:49:14 +0330

    runtime.py

--- runtime.py
+++ runtime.py
@@ -1,1 +1,20 @@
+class Environment:
+    def __init__(self, outer=None):
+        self.store = {}
+        self.outer = outer
+
+    def get(self, name):
+        if name in self.store:
+            return self.store[name]
+        elif self.outer:
+            return self.outer.get(name)
+        else:
+            raise Exception(f"Undefined variable '{name}'")
+
+    def set(self, name, value):
+        self.store[name] = value
+        return value
+
+def truthy(value):
+    return bool(value)
 
