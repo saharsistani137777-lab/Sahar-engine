5153209fae85949584372144fb2a9ffa768f30cc
Sahar Sistani <saharsistani137777@gmail.com>
2025-11-23 12:40:17 +0330

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -1,1 +1,52 @@
+from enum import Enum, auto
+
+class TokenType(Enum):
+    # Arithmetic
+    PLUS = '+'
+    MINUS = '-'
+    STAR = '*'
+    SLASH = '/'
+
+    # Delimiters
+    LPAREN = '('
+    RPAREN = ')'
+    LBRACE = '{'
+    RBRACE = '}'
+    SEMI = ';'
+    COMMA = ','
+
+    # Assignment & comparison
+    ASSIGN = '='
+    EQ = '=='
+    NEQ = '!='
+    LT = '<'
+    GT = '>'
+    LTE = '<='
+    GTE = '>='
+
+    # Keywords
+    LET = auto()
+    PRINT = auto()
+    IF = auto()
+    ELSE = auto()
+    WHILE = auto()
+    TRUE = auto()
+    FALSE = auto()
+
+    # Literals & identifiers
+    IDENT = auto()
+    NUMBER = auto()
+    STRING = auto()
+    
+    EOF = auto()
+
+class Token:
+    def __init__(self, type_, value=None, line=1, col=1):
+        self.type = type_
+        self.value = value
+        self.line = line
+        self.col = col
+
+    def __repr__(self):
+        return f"Token({self.type}, {self.value}, {self.line}:{self.col})"
 
